aea1f786ee09fe22a4332959ac9c65053b202f31
Joseph30070s <josephrp03@gmail.com>
2025-08-26 10:09:30 -0500

    Reemplazada exportación completa de la base de datos biblioteca.sql

--- database/biblioteca.sql
+++ database/biblioteca.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 21-08-2025 a las 20:48:15
+-- Tiempo de generación: 26-08-2025 a las 17:05:50
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
